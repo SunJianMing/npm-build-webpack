66e1031845e64b8616a5037177758aa67696dde4
Mr.sun <18273965210@163.com>
2020-09-21 10:31:41 +0800

    test: webpac-base test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -3,7 +3,7 @@ describe('webpack.base.js test case',()=>{
     const baseConfig = require('../../lib/webpack.base')
     console.log(baseConfig)
     it('entry',()=>{
-        assert.equal(baseConfig.entry.home,'/Users/sunjianming/Desktop/github/webpack/mr_b_w/test/smoke/template/src/views/home/index.js')
-        // assert.equal(baseConfig.entry.about,'/Users/sunjianming/Desktop/github/webpack/mr_b_w/test/smoke/template/src/views/about/index.js')
+        assert.equal(baseConfig.entry.home.indexOf('mr_b_w/test/smoke/template/src/views/home/index.js')>-1,true)
+        assert.equal(baseConfig.entry.about.indexOf('mr_b_w/test/smoke/template/src/views/about/index.js')>-1,true)
     })
 })
