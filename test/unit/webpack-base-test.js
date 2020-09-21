38256c827fc7dda681f117ba75cbd31d634251dd
Mr.sun <18273965210@163.com>
2020-09-21 10:34:31 +0800

    test: webpack.base test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -1,9 +1,8 @@
 const assert = require('assert')
 describe('webpack.base.js test case',()=>{
     const baseConfig = require('../../lib/webpack.base')
-    console.log(baseConfig)
     it('entry',()=>{
-        assert.equal(baseConfig.entry.home.indexOf('mr_b_w/test/smoke/template/src/views/home/index.js')>-1,true)
-        assert.equal(baseConfig.entry.about.indexOf('mr_b_w/test/smoke/template/src/views/about/index.js')>-1,true)
+        assert.equal(baseConfig.entry.home.indexOf('test/smoke/template/src/views/home/index.js')>-1,true)
+        assert.equal(baseConfig.entry.about.indexOf('test/smoke/template/src/views/about/index.js')>-1,true)
     })
 })
